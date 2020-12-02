3cd053de351397e77d3f83b29f6d7a24c8669008
Mohit Gupta <mohitg593@gmail.com>
2020-12-02 19:55:32 +0530

    Uncontrolled Forms

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import {Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col, Row, FormFeedback} from 'reactstrap';
+import {Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col, FormFeedback} from 'reactstrap';
 import {Link} from 'react-router-dom';
-import { PROMOTIONS } from '../shared/promotions';
 
 class Contact extends Component {
     constructor(props){
