319077b9c8db51237010bb5bbb2115e25eef0659
Mohit Gupta <mohitg593@gmail.com>
2020-12-05 16:51:50 +0530

    React project

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Breadcrumb, BreadcrumbItem, Button, Label, Row, Col} from 'reactstrap';
 import {Link} from 'react-router-dom';
-import {Control, Form, Errors, actions} from 'react-redux-form';
+import {Control, Form, Errors} from 'react-redux-form';
 
 const required= (val) => val && val.length;
 const maxLength= (len) => (val) => !(val) || (val.length <= len);
@@ -16,9 +16,9 @@ class Contact extends Component {
     }
 
     handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
-        this.props.resetFeedbackForm();
+        alert('Thankyou for your feedback! ' + JSON.stringify(values));
+        this.props.postFeedback(this.props.newFeedback, values.firstname, values.lastname, values.telnum,
+            values.email, values.message);
     }
 
 
